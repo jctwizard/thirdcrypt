90cac946add07d3ec0bfd6a38e8e410382413b90
James Wood <jamie_wood@ymail.com>
2017-07-27 23:25:18 +0100

    Add texture and 64 by 64 resolution

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 setTimeout(function() { window.scrollTo(0, 1) }, 100);
 
-var camera, scene, renderer;
-var mesh;
+var camera, scene, renderer, bufferTexture, planeScene, planeCamera;
+var mesh, planeMesh;
 
 init();
 animate();
@@ -12,16 +12,32 @@ function init()
 	camera.position.z = 400;
 	scene = new THREE.Scene();
 	var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
-	var material = new THREE.MeshBasicMaterial();
+  var texture = new THREE.TextureLoader().load( 'images/rock.jpg');
+  texture.minFilter = THREE.NearestFilter;
+  texture.magFilter = THREE.NearestFilter;
+  var material = new THREE.MeshBasicMaterial({map: texture});
 	mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
-	renderer = new THREE.WebGLRenderer();
+
+  bufferTexture = new THREE.WebGLRenderTarget( 64, 64, { minFilter: THREE.NearestFilter, magFilter: THREE.NearestFilter});
+
+  planeCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 1, 1000);
+  planeCamera.position.z = 1;
+  planeScene = new THREE.Scene();
+  var plane = new THREE.PlaneGeometry(2, 2);
+  var planeMaterial = new THREE.MeshBasicMaterial({map:bufferTexture})
+  planeMesh = new THREE.Mesh(plane, planeMaterial);
+  planeScene.add( planeMesh );
+
+  // Create the texture that will store our result
+  renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
   renderer.domElement.id = "viewport";
 	document.body.appendChild( renderer.domElement );
 	//
 	window.addEventListener( 'resize', onWindowResize, false );
+
 }
 
 function onWindowResize()
@@ -36,5 +52,6 @@ function animate()
 	requestAnimationFrame( animate );
 	mesh.rotation.x += 0.005;
 	mesh.rotation.y += 0.01;
-	renderer.render( scene, camera );
+  renderer.render(scene, camera, bufferTexture);
+	renderer.render(planeScene, planeCamera);
 }
